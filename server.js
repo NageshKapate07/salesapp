1d9234742b9a61919120ded4e338cc0e50995948
NageshKapate07 <104498236+NageshKapate07@users.noreply.github.com>
2022-07-28 11:14:41 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+var express=require('express');
+var app=express();
+
+app.get("/",(req, res)=>{
+    res.send(
+            "<h1>Vijay Sales </h1>"
+            +"<hr/>"
+            +"<h3>Smart Devices for sale</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Laptops</li>"
+            +"<li>Mobile Phones</li>"
+            +"<li>Hololgraphic Devices</li>"
+            +"<li>Samrt Watches</li>"
+            +"<li>Gaming Consoles</li>"
+            + "</ol>"
+    );
+});
+app.get("/aboutus",(req, res)=>{
+    res.send(
+            "<h1>Vijay Sales </h1>"
+            +"<hr/>"
+            +"<h3>Pride In Excellence</h3>"
+            + "<br/>"
+            + "<ol>"
+            +"<li>Chief Mentor: Nagesh Kapate</li>"
+            +"<li>Director: Nikita Patil</li>"
+            +"<li>Subject Matter Expert: Shraddha kale</li>"
+            + "</ol>"
+    );
+});
+
+app.get("/login",(req, res)=>{
+    res.send(
+            "<h1>Vijay Sales </h1>"
+            + "<h3>Login</h3>"
+            +"<hr/>"
+            +"<form>"
+            + "<input/>"
+            + "<br/>"
+            +"<input/> "
+            +"<button>login</button>"
+            + "</form>"
+    );
+});
+
+var server=app.listen(9000);
+console.log("Vijay Sales Online shopping running on port 9000");
